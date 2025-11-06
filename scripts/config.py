a69861b6815b590b8b37bd782a74c2738bb4567e
Antoine R <antoine.robins18@gmail.com>
2025-11-06 18:45:21 +0100

    add data paths

--- scripts/config.py
+++ scripts/config.py
@@ -1,1 +1,7 @@
+import os
+
 INPUT_PATH = r"\\MYCLOUDEX2ULTRA\Vidéos\Films"
+
+DATA_PATH = r"c:\\Users\\Antoine_R\\Documents\\1) Code\\Scraping infos films\\data"
+INTERMEDIATE_PATH = os.path.join(DATA_PATH, "intermediate")
+FINAL_PATH = os.path.join(DATA_PATH, "final")
