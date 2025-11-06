278ff77bfdf6835a288e8ec2c21a42a52da720b8
Antoine R <antoine.robins18@gmail.com>
2025-11-06 11:57:11 +0100

    initialisation

---
+++ scripts/config.py
@@ -0,0 +1,1 @@
+INPUT_PATH = r"\\MYCLOUDEX2ULTRA\Vidéos\Films"
---
+++ scripts/main.py
@@ -0,0 +1,13 @@
+# %% Import packages
+import os
+
+# %% Import paths
+from config import INPUT_PATH
+
+# %% Import useful functions
+
+
+# %% Main
+os.listdir(INPUT_PATH)
+
+# %% Core
